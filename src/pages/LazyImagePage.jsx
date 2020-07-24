f57d196e7daa14a9ff5204b5ce9ae953aac69567
sleeplesseditor <josh@bryson.me.uk>
2020-07-24 23:30:03 +0100

    File structure updated

---
+++ src/pages/LazyImagePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './LazyImagePage.scss';
+
+const GridPage = () => {
+
+    return (
+        <div className="image-page">
+            <h1>Lazy Load Images</h1>
+        </div>
+    )
+}
+
+export default GridPage
